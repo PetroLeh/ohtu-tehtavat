e5b76f64e8457634ee5fe7b1c0ff39a1b160e11d
Petro Lehtonen <petro.lehtonen@gmail.com>
2021-11-22 10:31:04 +0200

    muokattu luokkaa Player

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -12,7 +12,13 @@ def main():
 
     for player_dict in response:
         player = Player(
-            player_dict['name']
+            player_dict['name'],
+            player_dict['nationality'],
+            player_dict['assists'],
+            player_dict['goals'],
+            player_dict['penalties'],
+            player_dict['team'],
+            player_dict['games']
         )
 
         players.append(player)
